f73f3bd1c320ecceb150b4a0a33a6a3e7dc455ac
Erika V. Miguel <emiguel@ford-243-10.local>
2014-11-22 19:52:21 -0500

    Initial Commit

---
+++ parsing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parsing
+//
+//  Created by Erika V. Miguel on 11/22/14.
+//  Copyright (c) 2014 Erika V. Miguel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
